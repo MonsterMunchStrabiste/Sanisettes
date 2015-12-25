a3fc37209ff55cf1b20f9e848ed136a4d4f136a5
MonsterMunchStrabiste <alexandre.rastel@gmail.com>
2015-12-25 01:25:56 +0100

    premier commit version fonctionnelle

---
+++ app/src/main/java/com/example/alexandre/myslidingtabs/activities/MainActivity.java
@@ -0,0 +1,176 @@
+package com.example.alexandre.myslidingtabs.activities;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.alexandre.myslidingtabs.R;
+import com.example.alexandre.myslidingtabs.classes.NaturalOrderComparator;
+import com.example.alexandre.myslidingtabs.service.GetWCServices;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    private ArrayList<String> lines;
+    private JSONArray WCs;
+
+    //public SlidingTabsColorsFragment.SampleFragmentPagerAdapter adapter;
+
+    // Whether the Log Fragment is currently shown
+    //private boolean mLogShown;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        IntentFilter intentFilter = new IntentFilter(WC_UPDATE);
+        LocalBroadcastManager.getInstance(this).registerReceiver(new WCUpdate(), intentFilter);
+        GetWCServices.startActionBiers(this);
+        setNewLines();
+
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            SlidingTabsColorsFragment fragment = new SlidingTabsColorsFragment();
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("lines", lines);
+            fragment.setArguments(bundle);
+            transaction.replace(R.id.sample_content_fragment, fragment);
+            transaction.commit();
+        }
+
+
+        /*adapter = new SlidingTabsColorsFragment.SampleFragmentPagerAdapter(getWCFromFile());
+        rv_biers.setAdapter(adapter);*/
+        /*ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);*/
+        //SlidingTabsColorsFragment.SampleFragmentPagerAdapter adapter = new SlidingTabsColorsFragment.SampleFragmentPagerAdapter(getWCFromFile());
+        //adapter.setNewLines();
+    }
+
+    public static final String WC_UPDATE = "WC_UPDATE";
+    public class WCUpdate extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, " ");//getIntent().getAction());
+            GetWCServices.startActionBiers(getApplicationContext());
+            setNewLines();
+            //new SlidingTabsColorsFragment.SampleFragmentPagerAdapter.setNewLines();
+        }
+    }
+
+    public JSONArray getWCFromFile() {
+        try {
+            InputStream is = new FileInputStream(getCacheDir()+"/"+"sanisettes.json");
+            byte[] buffer = new byte[is.available()];
+            is.read(buffer);
+            is.close();
+            //File chacheDir = getCodeCacheDir();
+            return new JSONArray(new String(buffer, "UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new JSONArray();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return new JSONArray();
+        }
+    }
+
+    public void setNewLines() {
+        WCs = getWCFromFile();
+
+        lines = new ArrayList<String>();
+        for (int i = 0; i < WCs.length(); i++) {
+            try {
+                String fields = WCs.getJSONObject(i).getString("fields");
+                JSONObject inside = new JSONObject(fields);
+                lines.add(inside.getString("ligne"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        Set uniqueLines = new HashSet<String>(lines);
+        //this.lines = Collections.sort(uniqueLines);
+        this.lines = new ArrayList<String>(uniqueLines);
+        Collections.sort(this.lines, new NaturalOrderComparator());
+        Log.d(TAG, "Fichier rechargé");
+        //notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        /*MenuItem logToggle = menu.findItem(R.id.menu_toggle_log);
+        logToggle.setVisible(findViewById(R.id.sample_output) instanceof ViewAnimator);
+        logToggle.setTitle(mLogShown ? R.string.sample_hide_log : R.string.sample_show_log);*/
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /*switch(item.getItemId()) {
+            case R.id.menu_toggle_log:
+                mLogShown = !mLogShown;
+                ViewAnimator output = (ViewAnimator) findViewById(R.id.sample_output);
+                if (mLogShown) {
+                    output.setDisplayedChild(1);
+                } else {
+                    output.setDisplayedChild(0);
+                }
+                supportInvalidateOptionsMenu();
+                return true;
+        }*/
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/alexandre/myslidingtabs/service/GetWCServices.java
@@ -0,0 +1,109 @@
+package com.example.alexandre.myslidingtabs.service;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.example.alexandre.myslidingtabs.activities.MainActivity;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class GetWCServices extends IntentService {
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_GET_ALL_BIERS = "com.example.alexandre.myslidingtabs.GET_ALL_BIERS";
+    private static final String TAG = "GetWCServices";
+
+    // TODO: Rename parameters
+    /*private static final String EXTRA_PARAM1 = "orf.esiea.villa_rastel.progmobile.extra.PARAM1";
+    private static final String EXTRA_PARAM2 = "orf.esiea.villa_rastel.progmobile.extra.PARAM2";*/
+
+    public GetWCServices() {
+        super("GetWCServices");
+    }
+
+    /**
+     * Starts this service to perform action GET_ALL_BIERS with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionBiers(Context context) {
+        Intent intent = new Intent(context, GetWCServices.class);
+        intent.setAction(ACTION_GET_ALL_BIERS);
+        /*intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);*/
+        context.startService(intent);
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_GET_ALL_BIERS.equals(action)) {
+                handleActionBiers();
+            }
+        }
+    }
+
+    /**
+     * Handle action GET_ALL_BIERS in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionBiers() {
+        // TODO: Handle action GET_ALL_BIERS
+        URL url = null;
+        try {
+            url = new URL("http://data.ratp.fr/explore/dataset/sanitaires-reseau-ratp/download/?format=json&timezone=Europe/Berlin");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            if(HttpURLConnection.HTTP_OK == conn.getResponseCode()) {
+                copyInputStreamToFile(conn.getInputStream(), new File(getExternalCacheDir(), "sanisettes.json"));
+                Log.d(TAG, "json download");
+                LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(MainActivity.WC_UPDATE));
+            }
+        } /*catch (MalformedURLException e) {
+            e.printStackTrace();
+        }*/ catch (IOException e) {
+            e.printStackTrace();
+        }
+        //throw new UnsupportedOperationException("Not yet implemented");
+
+    }
+
+    private void copyInputStreamToFile(InputStream inputStream, File file) {
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len= inputStream.read(buf))>0) {
+                out.write(buf,0,len);
+            }
+            out.close();
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
